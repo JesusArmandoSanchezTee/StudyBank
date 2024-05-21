09f5edc04e09a6072965404040f4bf2132031730
Alucard <jesusarmand20@gmail.com>
2024-05-21 00:43:59 -0500

    Modelos y json

---
+++ lib/core/domain/tansaccion_Model.dart
@@ -0,0 +1,13 @@
+class Transaccion {
+  final String fecha;
+  final String descripcion;
+  final double monto;
+  final double saldo;
+
+  Transaccion({
+    required this.fecha,
+    required this.descripcion,
+    required this.monto,
+    required this.saldo,
+  });
+}
